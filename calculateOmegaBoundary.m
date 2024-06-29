99b3e46ac2c29d43e477418b53a0780bc9823c69
sebastianschanz <mail@sebastianschanz.com>
2024-06-29 16:16:42 +0200

    asdf

--- calculateOmegaBoundary.m
+++
@@ -1,7 +0,0 @@
-function Omega = calculateOmegaBoundary(Omega, Psi, nx, ny, h)
-    % Randbedingungen für Omega setzen basierend auf Psi
-    Omega(:, 1) = -2 * Psi(:, 2) / h^2;
-    Omega(:, end) = -2 * Psi(:, end-1) / h^2;
-    Omega(1, :) = -2 * Psi(2, :) / h^2;
-    Omega(end, :) = -2 * Psi(end-1, :) / h^2;
-end
