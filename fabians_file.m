7098a1fdf154872f23f0718fb4a96e7d0c8c2e17
sebastianschanz <mail@sebastianschanz.com>
2024-07-01 15:52:22 +0200

    asdfaögds

---
+++ fabians_file.m
@@ -0,0 +1,1 @@
+asdfgadglkagölksj
--- tgv_02_initSimulation.m
+++ tgv_02_initSimulation.m
@@ -3,7 +3,8 @@ function [X, Y, Psi_0, Omega_0] = tgv_02_initSimulation(options)
     x = linspace(0, 2*pi, options.x_nr);  % Diskretisierung in x-Richtung
     y = linspace(0, 2*pi, options.y_nr);  % Diskretisierung in y-Richtung
     [X, Y] = meshgrid(x, y);  % Erzeugung des Gitters
-    Omega_0 = sin(X).*sin(Y);
+    F
+    Omega_0 = 2*sin(X).*sin(Y)*exp();
     Omega_0 = tgv_applyBoundaryConditions(Omega_0, 'Dirichlet'); % Randbedingungen für Omega anwenden
     Psi_0 = tgv_poissonSolver(Omega_0);  % Berechnung der Stromfunktion aus der Wirbelstärke
     Psi_0 = tgv_applyBoundaryConditions(Psi_0, 'Dirichlet'); % Randbedingungen für Psi anwenden
