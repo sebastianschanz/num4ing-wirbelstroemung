f5f1ad1f94ded3530f6d9e2241c00513f863cb20
sebastianschanz <mail@sebastianschanz.com>
2024-07-20 21:53:39 +0200

    writeGif, calcErrors, showTraj Optionen hinzugefügt

--- tgv_01_configureSimulation.m
+++ tgv_01_configureSimulation.m
@@ -1,6 +1,6 @@
 function options = tgv_01_configureSimulation()
     % Funktion zur Konfiguration der Simulationsparameter
-    options.nu = 0.01; % Kinematische Viskosität
+    options.nu = 0.08; % Kinematische Viskosität
     options.nx = 50; % Anzahl der Gitterpunkte in x-Richtung
     options.ny = 50; % Anzahl der Gitterpunkte in y-Richtung
 
@@ -17,12 +17,16 @@ function options = tgv_01_configureSimulation()
     options.dy = (options.y_max - options.y_min) / (options.nx - 1);
 
     % Endzeit, Anzahl der Zeitschritte und Zeitschrittweite
-    options.t_end = 5;
-    options.t_nr = 100;
+    options.t_end = 10;
+    options.t_nr = 200;
     options.dt = options.t_end / options.t_nr;
  
     options.method = 'expliziter-euler'; % Integrationsmethode
-    options.colormap = 'parula';         % Colormap für die Plots
-    options.writeGif = false;             % GIF-Datei schreiben
-    options.filename = 'C:\Users\Sebastian\Documents\00-dev\num4ing\num4ing-wirbelstroemung\export\simulation.gif'; % Dateiname für die GIF-Datei
+    options.writeGif = true;            % GIF-Datei schreiben
+    options.calcErrors = true;  % Fehlerberechnung aktivieren
+    options.showTraj = true;    % Bahnlinie eines Partikels anzeigen
+    options.particleIdx = 915; % Startindex für die Partikel
+    options.colormap = 'parula';         % Farbkarte für die Darstellung
+    options.nticks = 3;                  % Anzahl der Ticks auf Achsen
+    options.filename = 'C:\Users\Sebastian\Documents\00-dev\num4ing\num4ing-wirbelstroemung\export\simulation_errors.gif'; % Dateiname für die GIF-Datei
 end
