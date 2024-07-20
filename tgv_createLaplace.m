2a2291106df762994e89c7db625c9f51928b4b8b
sebastianschanz <mail@sebastianschanz.com>
2024-07-20 21:57:01 +0200

    Eigener LaPlace Operator. Für mich machts grob Sinn. Schaut mal drüber und in das verlinkte PDF.

--- tgv_createLaplace.m
+++ tgv_createLaplace.m
@@ -1,5 +1,7 @@
 function [D2x, D2y] = tgv_createLaplace(options)
     % Erstellt die Differenzenmatrizen für die zweite Ableitung in x- und y-Richtung
+    % Tensormethode nach: https://www.math.uci.edu/~chenlong/226/FDMcode.pdf
+    
     nx = options.nx;
     ny = options.ny;
     dx = options.dx;
@@ -7,6 +9,7 @@ function [D2x, D2y] = tgv_createLaplace(options)
     N = nx * ny; % Gesamtanzahl der Gitterpunkte
 
     % Speicherreservierung für die Differenzenmatrizen
+    % Erstellt dünnbesetzte Matrizen D2x, D2y der Größe N x N mit 5 * N Elementen
     D2x = spalloc(N, N, 5 * N);
     D2y = spalloc(N, N, 5 * N);
 
@@ -20,40 +23,32 @@ function [D2x, D2y] = tgv_createLaplace(options)
     D2y = spdiags([e -2*e e], [-1 0 1], N, N) / dy^2;
 
     % Vorwärtsdifferenzen am linken Rand (erste Spalte)
-    for i = 1:ny
-        D2x(i, i) = 1 / dx^2;
-        D2x(i, i + ny) = -2 / dx^2;
-        D2x(i, i + 2 * ny) = 1 / dx^2;
-        if i <= ny
-            D2y(i, i) = 1 / dy^2;
-            D2y(i, i + 1) = -2 / dy^2;
-            D2y(i, i + 2) = 1 / dy^2;
-        end
-    end
+    left_edge = (1:ny)';
+    D2x(sub2ind([N, N], left_edge, left_edge)) = 1 / dx^2; % Setzt diagonales Element am rechten Rand von D2x auf 1/dx^2
+    D2x(sub2ind([N, N], left_edge, left_edge + ny)) = -2 / dx^2; % Setzt Element rechts neben dem diagonalen Element auf -2/dx^2
+    D2x(sub2ind([N, N], left_edge, left_edge + 2 * ny)) = 1 / dx^2; % Setzt Element rechts neben dem Element rechts neben dem diagonalen Element auf 1/dx^2
+    D2y(sub2ind([N, N], left_edge, left_edge)) = 1 / dy^2; % Setzt diagonales Element am rechten Rand von D2y auf 1/dy^2
+    D2y(sub2ind([N, N], left_edge, left_edge + 1)) = -2 / dy^2; % Setzt Element rechts neben dem diagonalen Element auf -2/dy^2
+    D2y(sub2ind([N, N], left_edge, left_edge + 2)) = 1 / dy^2; % Setzt Element rechts neben dem Element rechts neben dem diagonalen Element auf 1/dy^2
 
     % Rückwärtsdifferenzen am rechten Rand (letzte Spalte)
-    for i = N - ny + 1:N
-        D2x(i, i) = 1 / dx^2;
-        D2x(i, i - ny) = -2 / dx^2;
-        D2x(i, i - 2 * ny) = 1 / dx^2;
-    end
-    for i = ny:ny:N
-        D2y(i, i) = 1 / dy^2;
-        D2y(i, i - 1) = -2 / dy^2;
-        D2y(i, i - 2) = 1 / dy^2;
-    end
+    right_edge = (N - ny + 1:N)';
+    D2x(sub2ind([N, N], right_edge, right_edge)) = 1 / dx^2; % Setzt diagonales Element am rechten Rand von D2x auf 1/dx^2
+    D2x(sub2ind([N, N], right_edge, right_edge - ny)) = -2 / dx^2; % Setzt Element links neben dem diagonalen Element auf -2/dx^2
+    D2x(sub2ind([N, N], right_edge, right_edge - 2 * ny)) = 1 / dx^2; % Setzt Element links neben dem Element links neben dem diagonalen Element auf 1/dx^2
+    D2y(sub2ind([N, N], right_edge, right_edge)) = 1 / dy^2; % Setzt diagonales Element am rechten Rand von D2y auf 1/dy^2
+    D2y(sub2ind([N, N], right_edge, right_edge - 1)) = -2 / dy^2; % Setzt Element links neben dem diagonalen Element auf -2/dy^2
+    D2y(sub2ind([N, N], right_edge, right_edge - 2)) = 1 / dy^2; % Setzt Element links neben dem Element links neben dem diagonalen Element auf 1/dy^2
 
     % Vorwärtsdifferenzen am unteren Rand (erste Zeile)
-    for i = 1:nx:ny
-        D2y(i, i) = 1 / dy^2;
-        D2y(i, i + 1) = -2 / dy^2;
-        D2y(i, i + 2) = 1 / dy^2;
-    end
+    bottom_edge = (1:ny:N)';
+    D2y(sub2ind([N, N], bottom_edge, bottom_edge)) = 1 / dy^2; % Setzt diagonales Element am rechten Rand von D2y auf 1/dy^2
+    D2y(sub2ind([N, N], bottom_edge, bottom_edge + 1)) = -2 / dy^2; % Setzt Element rechts neben dem diagonalen Element auf -2/dy^2
+    D2y(sub2ind([N, N], bottom_edge, bottom_edge + 2)) = 1 / dy^2; % Setzt Element rechts neben dem Element rechts neben dem diagonalen Element auf 1/dy^2
 
     % Rückwärtsdifferenzen am oberen Rand (letzte Zeile)
-    for i = N - ny + 1:N
-        D2y(i, i) = 1 / dy^2;
-        D2y(i, i - 1) = -2 / dy^2;
-        D2y(i, i - 2) = 1 / dy^2;
-    end
+    top_edge = (ny:ny:N)';
+    D2y(sub2ind([N, N], top_edge, top_edge)) = 1 / dy^2; % Setzt diagonales Element am rechten Rand von D2y auf 1/dy^2
+    D2y(sub2ind([N, N], top_edge, top_edge - 1)) = -2 / dy^2; % Setzt Element links neben dem diagonalen Element auf -2/dy^2
+    D2y(sub2ind([N, N], top_edge, top_edge - 2)) = 1 / dy^2; % Setzt Element links neben dem Element links neben dem diagonalen Element auf 1/dy^2
 end
