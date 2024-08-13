e5dc53101c1eaa6c5c8d3a72df449971fb1595ad
sebastianschanz <mail@sebastianschanz.com>
2024-08-13 20:59:41 +0200

    dynamisches Filenaming... ;-)

--- tgv_configureSimulation.m
+++ tgv_configureSimulation.m
@@ -1,6 +1,6 @@
 function options = tgv_configureSimulation()
     % Funktion zur Konfiguration der Simulationsparameter
-    options.nu = 0.005; % Kinematische Viskosität
+    options.nu = 0.1; % Kinematische Viskosität
     options.rho = 1.0; % Dichte des Fluids
     options.mu = options.nu * options.rho; % Dynamische Viskosität
     options.nx = 50; % Anzahl der Gitterpunkte in x-Richtung
@@ -19,10 +19,10 @@ function options = tgv_configureSimulation()
     options.dy = (options.y_max - options.y_min) / (options.nx - 1);
 
     % Endzeit, Anzahl der Zeitschritte und Zeitschrittweite
-    options.t_end = 8;
+    options.t_end = 6;
     options.t_nr = options.t_end*20+1;
     options.dt = options.t_end / options.t_nr;
-    options.stepMethod = 'explicitEuler';       % Methode für Schrittverfahren
+    options.stepMethod = 'heun';       % Methode für Schrittverfahren
     options.Aufwind = false;                     % Aufwind Methode Anwenden oder nicht
     options.max_iter = 100;                     % Maximale Anzahl von Iterationen für implizite Verfahren
     options.tol = 1e-6;                        % Toleranz für die Iteration der impliziten Verfahren
@@ -33,12 +33,13 @@ function options = tgv_configureSimulation()
     options.showTraj = false;               % Bahnlinie eines Partikels anzeigen
     options.numParticles = 12;              % Startindex für die Partikel
     
-    options.writeGif = false;               % GIF-Datei schreiben
+    options.writeGif = true;                % GIF-Datei schreiben
     options.colormap = 'parula';            % Farbkarte für die Darstellung
     options.nticks = 3;                     % Anzahl der Ticks auf Achsenen
     options.imgWidth = 400;                 % Bildbreite
     options.imgHeight = options.imgWidth;   % Bildhöhe
     options.fontSize = 8;                   % Grund-Schriftgröße
-    options.imgScale = 1.0;                   % 1-1.8, Skalierungsfaktor für Bildauflösung, hochschrauben für schönere Gifs
-    options.filename = '.\export\simulation_enstrophy_8_nu_0.1.gif'; % Dateiname für die GIF-Datei
+    options.imgScale = 1.5;                   % 1-1.8, Skalierungsfaktor für Bildauflösung, hochschrauben für schönere Gifs
+    options.filename = sprintf('.\\export\\simulation_%s_nu_%.2f_tend_%d.gif', ...
+                                options.stepMethod, options.nu, options.t_end);
 end
